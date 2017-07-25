bf8ab180616c3e816eb80209327c49aff64fc30c
Ilya Pyatkin <baldurmods@gmail.com>
2017-07-25 23:15:06 +0300

    Binded products with users
    Non superuser admin panel users now see only their products on the product page.

--- Fresh/admin.py
+++ Fresh/admin.py
@@ -5,6 +5,7 @@ from cartridge.shop.models import Sale, DiscountCode
 from mezzanine.pages.admin import PageAdmin
 from mezzanine.generic.models import ThreadedComment
 from mezzanine.conf.models import Setting
+from Fresh.models import UserProduct
 
 
 # Remove "content" field from cartridge's category edit page.
@@ -15,8 +16,38 @@ CategoryAdmin.fieldsets = category_fieldsets
 
 # Edit displayable and editable fields of the product page
 ProductAdmin.list_display = ["admin_thumb", "title", "status", "available", "sku", 
-							 "unit_price", "num_in_stock", "admin_link"]
+							 "unit_price", "num_in_stock", "userproduct", "admin_link"]
 ProductAdmin.list_editable = ["status", "available", "unit_price", "num_in_stock"]
+
+
+# Add filter based on groups to product page
+ProductAdmin.list_filter = ("status", "available", "categories", "userproduct__owner__groups")
+
+def lookup_allowed(self, key, value):
+	return True
+
+ProductAdmin.lookup_allowed = lookup_allowed
+
+
+# Create a UserProduct object when a new Product is saved
+def new_save_model(self, request, obj, form, change):
+    super(ProductAdmin, self).save_model(request, obj, form, change)
+    if not change:
+    	UserProduct.objects.create(product=obj, owner=request.user)
+    self._product_id = obj.id
+
+ProductAdmin.save_model = new_save_model
+
+
+# Hide items that are not owned by the current user
+# if the user doesn't have super user privileges
+def product_get_queryset(self, request):
+	qs = super(ProductAdmin, self).get_queryset(request)
+	if request.user.is_superuser:
+		return qs
+	return qs.filter(userproduct__owner=request.user)
+
+ProductAdmin.get_queryset = product_get_queryset
 
 
 # Hide unused pages
--- Fresh/models.py
+++ Fresh/models.py
@@ -1,4 +1,9 @@
 from cartridge.shop.models import Product
+from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from django.utils.translation import ugettext, ugettext_lazy as _
+from django.contrib.auth.models import User
 
 
 # Show "Coming soon" on items that are not in stock
@@ -6,3 +11,23 @@ def in_stock(self):
 	return self.num_in_stock is None or self.num_in_stock > 0 
 
 Product.in_stock = in_stock
+
+# Bind Product to a certain User
+class UserProduct(models.Model):
+	product = models.OneToOneField(
+		Product,
+		on_delete=models.CASCADE,
+		primary_key=True,
+	)
+	owner = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE,
+		verbose_name=_("Owner")
+	)
+
+	def __str__(self):
+		return str(self.owner)
+
+@receiver(post_delete, sender=UserProduct)
+def auto_delete_product(sender, instance, **kwargs):
+	instance.product.delete()
